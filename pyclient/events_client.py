6881633e75a62ac3a5ec43ab7a9b9e128ceeb86c
sinakhalili <khalili@sfu.ca>
2019-04-05 03:03:10 -0700

    event subscription working mostly

--- pyclient/events_client.py
+++ pyclient/events_client.py
@@ -1,14 +1,4 @@
 #! /usr/bin/env python3
-
-'''Sample Sawtooth event client
-
-   To run, start the validator then type the following on the command line:
-       ./events_client.py
-
-   For more information, see
-   https://sawtooth.hyperledger.org/docs/core/releases/latest/app_developers_guide/event_subscriptions.html
-'''
-
 import sys
 import traceback
 import zmq
@@ -16,19 +6,11 @@ from sawtooth_sdk.messaging.stream import Stream
 from sawtooth_sdk.protobuf import events_pb2
 from sawtooth_sdk.protobuf import client_event_pb2
 from sawtooth_sdk.protobuf.validator_pb2 import Message
-
-# hard-coded for simplicity (otherwise get the URL from the args in main):
-# For localhost access:
-#DEFAULT_VALIDATOR_URL = 'tcp://localhost:4004'
-# For Docker access:
 DEFAULT_VALIDATOR_URL = 'tcp://validator:4004'
 # Calculated from the 1st 6 characters of SHA-512("notary"):
 NOTARY_TP_ADDRESS_PREFIX = '58504b'
 
-
-def listen_to_events(delta_filters=None):
-    '''Listen to notary state-delta events.'''
-
+def listen_to_events(socket, delta_filters=None):
     # Subscribe to events
     block_commit_subscription = events_pb2.EventSubscription(
         event_type="sawtooth/block-commit")
@@ -80,46 +62,16 @@ def listen_to_events(delta_filters=None):
 
 def main():
     '''Entry point function for the client CLI.'''
+    ctx = zmq.Context()
+    socket = ctx.socket(zmq.DEALER)
+    socket.connect(DEFAULT_VALIDATOR_URL)
 
-    subscription = EventSubscription(
-        event_type="sawtooth/state-delta",
-        filters=[
-            EventFilter(
-                key="address",
-                match_string=
-                NOTARY_TP_ADDRESS_PREFIX + ".*",
-                filter_type=EventFilter.REGEX_ANY
-            )
-        ]
-    )
-
-
-#   subscription = EventSubscription(
-#     event_type="sawtooth/state-delta",
-#     filters=[
-#         # Filter to only addresses in the "xo" namespace using a regex
-#         EventFilter(
-#             key="address",
-#             match_string="5b7349.*",
-#             filter_type=EventFilter.REGEX_ANY)
-#     ])
     filters = [events_pb2.EventFilter(key="address",
                                       match_string=
                                       NOTARY_TP_ADDRESS_PREFIX + ".*",
                                       filter_type=events_pb2.
                                       EventFilter.REGEX_ANY)]
-
-    try:
-        # To listen to all events, pass delta_filters=None :
-        #listen_to_events(delta_filters=None)
-        listen_to_events(delta_filters=filters)
-    except KeyboardInterrupt:
-        pass
-    except SystemExit as err:
-        raise err
-    except BaseException as err:
-        traceback.print_exc(file=sys.stderr)
-        sys.exit(1)
+    listen_to_events(socket, delta_filters=filters)
 
 if __name__ == '__main__':
     main()
